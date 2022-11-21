9350142d3a580291a4a714e17cb1e972556be58d
Ali M. Alagrami <alialagrami@gmail.com>
2022-11-21 16:30:23 +0100

    testing done

--- exp_init.py
+++ exp_init.py
@@ -1,11 +1,7 @@
 from runner import experiment
 
 def main():
-<<<<<<< HEAD
-    my_expirments = experiment("conf3.yaml")
-=======
     my_expirments = experiment("test.yaml")
->>>>>>> 3a40ac2c0867cd3c5554ed03edc0b0856c6bfec9
     my_expirments.run()
     print("finised !")
 
